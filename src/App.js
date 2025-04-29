05e719dfb815c45e6bdea8441f0db117a989dfef
Mikołaj Frankiewicz <Frankiewicz.Mikolaj@zst.radom.pl>
2025-04-29 22:58:48 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import User from "./components/User";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Strona główna</Link></li>
+          <li><Link to="/about">O nas</Link></li>
+          <li><Link to="/contact">Kontakt</Link></li>
+          <li><Link to="/user/Janek">Przykład: Janek</Link></li>
+        </ul>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/user/:name" element={<User />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Contact.js
@@ -0,0 +1,4 @@
+export default function Contact() {
+    return <h1>Kontakt</h1>;
+  }
+  
